96717253238c8702141f11db396981e74ea649b2
khanhtran <khanhtpse173570>
2024-07-02 19:21:00 +0700

    update Service Policy

--- src/page/pageCustomer/ServicePolicy.jsx
+++ src/page/pageCustomer/ServicePolicy.jsx
@@ -3,132 +3,132 @@ import { Col, Image, Row } from "react-bootstrap";
 
 const ServicePolicy = () => {
   return (
-    <div>
+    <div style={{ textAlign: "justify" }}>
       <Row>
         <Col md="8" style={{ marginBottom: "50px" }}>
-          <h1 className="" style={{ marginTop: "50px", marginLeft: "600px", marginBottom:"50px" }}>
+          <h1 className="" style={{ marginTop: "50px", marginBottom: "50px", textAlign: "center" }}>
             Service Policy
           </h1>
-          <p style={{ marginLeft: "50px" }}>
-            <h2 className="text-center"> 1. QUI ĐỊNH CHUNG</h2>
+          <div style={{ paddingLeft: "50px", paddingRight: "50px" }}>
+            <h2 className="text-center"> 1. GENERAL RULES</h2>
             <div>
-              1.1. Khách hàng phải cung cấp đầy đủ và chính xác các thông tin ở
-              PHIẾU YÊU CẦU GIÁM ĐỊNH. DIAMOND VALUATION sẽ căn cứ vào PHIẾU YÊU
-              CẦU GIÁM ĐỊNH tiến hành tạo BIÊN NHẬN GIÁM ĐỊNH. Các thông tin:
-              Tên Khách hàng, địa chỉ, số điện thoại, Email (nếu có), yêu cầu
-              dịch vụ. Trường hợp Niêm phong lại hay Cấp lại Giấy Giám định,
-              Khách hàng cần cung cấp số số điện thoại của Mẫu Giám định (có thể
-              kèm theo Niêm phong hoặc Giấy Giám định cũ).
-            </div>
-            <div>
-              1.2. DIAMOND VALLUATION không chịu trách nhiệm pháp lý về tính xác
-              thực các thông tin do Khách hàng cung cấp.
+              1.1. Customers must provide complete and accurate information
+              INSPECTION REQUEST FORM. DIAMOND VALUATION will be based on LOVE VOUCHER
+              DEMAND INSPECTION proceeds to create an INSPECTION RECEIPT. Information:
+              Customer name, address, phone number, email (if any), request
+              service. In case of Re-sealing or Re-Issue of Inspection Certificate,
+              Customers need to provide the phone number of the Inspection Form (possible
+              enclosed with old Seal or Inspection Certificate).
             </div>
             <div>
-              1.3. BIÊN NHẬN GIÁM ĐỊNH được xem như HỢP ĐỒNG DỊCH VỤ GIÁM ĐỊNH
-              giữa DIAMOND VALUATIONvà Khách hàng. BNGĐ được in thành hai bản,
-              một bản do DIAMONDgiữ và một bản do Khách hàng giữ.
+              1.2. DIAMOND VALLUATION is not liable for accuracy
+              authenticate the information provided by the Customer.
             </div>
             <div>
-              1.4. Trường hợp Khách hàng mong muốn sự thay đổi HĐDVGĐ phải gọi
-              điện thông báo cho DIAMOND VALUATION trong vòng 6 giờ kể từ khi
-              gửi Mẫu (không áp dụng cho yêu cầu Giám định nhanh). Sau thời gian
-              này, Khách hàng sẽ không được thay đổi bất kỳ nội dung nào trên
-              HĐDVGĐ.
+              1.3. INSPECTION RECEIPT is considered an INSPECTION SERVICE CONTRACT
+              between DIAMOND VALUATION and Customer. The Board of Directors is printed in two copies,
+              One copy is kept by DIAMOND and one copy is kept by the Customer.
             </div>
             <div>
-              1.5. BNGĐ chỉ xác nhận đúng SỐ LƯỢNG Mẫu Giám định. Các thông tin
-              về trọng lượng, kích thước, màu sắc, dạng cắt mài..., tên Mẫu do
-              Khách hàng cung cấp, …chỉ mang tính chất tham khảo và nhận diện.
+              1.4. In case the Customer wishes to change the Family Service Agreement, he or she must call
+              Notify DIAMOND VALUATION within 6 hours of receipt
+              Send Samples (not applicable for Express Inspection requests). After time
+              Customers will not be allowed to change any of the above content
+              HDDVGP.
             </div>
             <div>
-              1.6. DIAMOND VALUATION không xác định bản chất thực tế Mẫu Giám
-              định là loại đá gì, thiên nhiên, tổng hợp, có xử lý hay không tại
-              thời điểm nhận Mẫu Giám định.
+              1.5. The Steering Committee only confirms the correct QUANTITY of Inspection Samples. The information
+              about weight, size, color, cut shape..., Model name due
+              Provided by customers, ...for reference and identification purposes only.
             </div>
             <div>
-              1.7. BNGĐ chỉ có giá trị trong vòng 30 ngày kể từ ngày gửi Mẫu
-              Giám định. Sau 30 ngày nếu Khách hàng vẫn chưa đến nhận kết quả .
-              Nếu trong khoảng thời gian này Khách hàng vẫn không đến lấy hoặc
-              khi BNGĐ đã hết giá trị mà DIAMOND VALUATION không liên hệ được với Khách hàng
-              thì DIAMOND VALUATION sẽ tiến hành lập biên bản niêm phong các Mẫu
-              Giám định và có hướng giải quyết theo Quy định của Công ty.
+              1.6. DIAMOND VALUATION does not determine the actual nature of Model Giam
+              Determine what type of stone it is, natural, synthetic, treated or not
+              time of receiving the Inspection Form.
             </div>
             <div>
-              1.8. Trong trường hợp việc Giám định có trở ngại hay cần thay đổi,
-              DIAMOND VALUATION sẽ gọi điện thông báo, bàn bạc với Khách hàng trên tinh thần
-              hợp tác, tôn trọng lẫn nhau để đưa ra hướng giải quyết tốt nhất.
+              1.7. BNGD is only valid for 30 days from the date of sending the Form
+              Inspection. After 30 days, if the Customer still has not received the results.
+              If during this time the Customer still does not come to pick up or
+              When the BNGD has no value and DIAMOND VALUATION cannot contact the Customer
+              DIAMOND VALUATION will prepare a record to seal the Samples
+              Inspect and have solutions according to the Company's regulations.
             </div>
             <div>
-              1.9. DIAMOND VALUATION thực hiện việc Giám định một cách độc lập và có quyền từ
-              chối yêu cầu Giám định khi Khách hàng không cung cấp đầy đủ thông
-              tin trên PHIẾU YÊU CẦU GIÁM ĐỊNH hoặc xét thấy không đảm bảo về
-              các yếu tố kỹ thuật, chuyên môn.
+              1.8. In case the appraisal has problems or needs to be changed,
+              DIAMOND VALUATION will call to inform and discuss with the Customer in spirit
+              Cooperate and respect each other to come up with the best solution.
             </div>
             <div>
-              1.10. Giấy Giám định của DIAMOND VALUATION không phải là giấy Bảo
-              hành, không định giá Mẫu Giám định đính kèm.
+              1.9. DIAMOND VALUATION performs the Inspection independently and has the right to
+              Refuse the Inspection request when the Customer does not provide sufficient information
+              information on the INSPECTION REQUEST FORM or deemed unwarranted
+              technical and professional factors.
             </div>
             <div>
-              1.11. DIAMOND VALUATION không giữ vai trò đại diện hay đảm bảo cho Giấy Giám
-              định vì Giấy giám định chỉ mô tả trung thực những đặc tính và các
-              thông số kỹ thuật của Mẫu Giám định sau khi đã được Giám định theo
-              đúng tiêu chuẩn và thiết bị hiện có của DIAMOND VALUATIONtại thời
-              điểm Giám định. Kết quả Giám định trong những lần Giám định sau có
-              thể thay đổi tùy thuộc vào: · Thời điểm, cách thức và Chuyên gia
-              thực hiện Giám định. · Những thay đổi và cải tiến công nghệ, thiết
-              bị dùng Giám định giúp các Chuyên gia phát hiện những yếu tố khác
-              của Mẫu mà trước đó DIAMOND VALUATION không phát hiện được.
+              1.10. DIAMOND VALUATION's Inspection Paper is not a Warranty Paper
+              Warranty, no valuation Appraisal form attached.
             </div>
             <div>
-              1.12. DIAMOND VALUATION sẽ không chịu trách nhiệm về pháp lý cho bất kì sự
-              tranh chấp, hư hại, kể cả những thiệt hại của Khách hàng xuất phát
-              từ lỗi sai sót trên Giấy Giám định và trong việc lưu hành Giấy
-              Giám định.
+              1.11. DIAMOND VALUATION does not represent or guarantee the Certificate
+              appraisal because the appraisal certificate only honestly describes the characteristics and functions
+              Technical specifications of the Inspection Form after it has been inspected
+              in accordance with DIAMOND VALUATION's existing standards and equipment at the time
+              Inspection point. Inspection results in subsequent inspections are available
+              May vary depending on: · Time, method and Expert
+              Perform Inspection. · Changes and improvements in technology and design
+              The device uses Appraisal to help Experts detect other factors
+              of Sample that was previously not detected by DIAMOND VALUATION.
             </div>
-            <div style={{marginBottom:"50px"}}>
-              1.13. Quy định Niêm phong lại (Seal lại) · DIAMOND VALUATION sẽ tiến hành Niêm
-              phong lại các Mẫu Giám định khi đã xác định đúng các thông số kĩ
-              thuật. Trường hợp các thông số kĩ thuật có sự sai lệch hoặc phát
-              sinh các lỗi trong quá trình sử dụng Mẫu từ phía Khách hàng (rạn,
-              nứt, mẻ cạnh, mẻ đáy, cắt, mài, đánh bóng,…) sẽ KHÔNG Niêm phong
-              lại và thông báo trả lại Mẫu Giám định cho Khách hàng. · Trường
-              hợp còn niêm phong cũ do DIAMOND VALUATION cung cấp mà tình trạng
-              Mẫu Giám định vẫn còn nguyên vẹn thì DIAMOND VALUATION sẽ mở niêm phong cũ và
-              niêm phong mới lại theo yêu cầu của Khách hàng. · Trường hợp còn
-              niêm phong cũ do DIAMOND VALUATION cung cấp nhưng tình trạng Mẫu
-              Giám định không còn nguyên vẹn (có dấu hiệu rạn, nứt, …) mà Khách
-              hàng vẫn có nhu cầu và đồng ý Niêm phong lại thì các Mẫu này phải
-              được kiểm tra tình trạng Mẫu và lập biên bản xác nhận tình trạng
-              Mẫu trước sự chứng kiến của đại diện DIAMOND VALUATION và Khách
-              hàng. Niêm phong cũ này phải do chính Khách hàng mở. Mọi rủi ro,
-              khiếu nại về sau đối với trường hợp này DIAMOND VALUATION hoàn toàn không chịu
-              trách nhiệm.
-            </div>
-            <h2  className="text-center">2. CAM KẾT CỦA KHÁCH HÀNG</h2>
-            <div style={{marginBottom:"50px"}}>
-            Khách hàng có trách nhiệm kiểm tra đúng số lượng Mẫu Giám định, chất
-            lượng niêm phong, số lượng và chất lượng Giấy Giám định (nếu có)
-            trước khi rời khỏi quầy giao dịch. Mọi khiếu nại của Khách hàng liên
-            quan đến các vấn đề nêu trên khi rời khỏi quầy giao dịch sẽ không
-            được giải quyết.
-            </div>
-            <h2 className="text-center">3. CAM KẾT CỦA DIAMOND VALUATION</h2>
             <div>
-              DIAMOND VALUATION cam kết thực hiện đúng nội dung Giám định đã
-              thỏa thuận với Khách hàng dựa trên kiến thức, năng lực kỹ thuật,
-              chuyên môn hiện có để cung cấp tốt nhất dịch vụ Giám định cho
-              Khách hàng với đầy đủ trách nhiệm của mình theo đúng các quy định
-              hiện hành. DIAMOND VALUATION cam kết bảo mật toàn bộ thông tin cá
-              nhân và kết quả Giám định của Khách hàng, không cung cấp thông tin
-              về việc Giám định cho bên thứ ba nào biết nếu không có sự đồng ý
-              bằng văn bản của Khách hàng, ngoại trừ các quy định khác của pháp
-              luật.
+              1.12. DIAMOND VALUATION will not be liable for any loss
+              disputes, damages, including Customer's damages, arise
+              from errors on the Inspection Paper and in the circulation of the Paper
+              Inspection.
             </div>
-          </p>
+            <div style={{ marginBottom: "50px" }}>
+              1.13. Re-sealing Regulations · DIAMOND VALUATION will conduct the Sealing
+              Re-seal the Inspection Forms when the correct parameters have been determined
+              art. In case there are discrepancies or errors in the technical specifications
+              errors arise during the use of the Sample from the Customer (cracks,
+              cracked, chipped edges, chipped bottom, cut, ground, polished, etc.) will NOT be sealed
+              return and notify the return of the Inspection Sample to the Customer. · School
+              Old sealed case provided by DIAMOND VALUATION but in good condition
+              If the Inspection Form is still intact, DIAMOND VALUATION will open the old seal and
+              Newly sealed according to Customer's request. · In case of remaining
+              old seal provided by DIAMOND VALUATION but Model condition
+              Inspection is no longer intact (shows signs of cracking, cracking, etc.)
+              If the customer still needs and agrees to re-sealing, these Forms must be
+              Check the sample's condition and make a record confirming the condition
+              Sample in the presence of DIAMOND VALUATION representatives and guests
+              row. This old seal must be opened by the Customer himself. All risks,
+              DIAMOND VALUATION will absolutely not accept future complaints in this case
+              responsibility.
+            </div>
+            <h2 className="text-center">2. CUSTOMER COMMITMENT</h2>
+            <div style={{ marginBottom: "50px" }}>
+              Customers are responsible for checking the correct quantity of Inspection Samples and quality
+              Seal quality, quantity and quality of Inspection Paper (if any)
+              before leaving the counter. All complaints from Customers are related
+              regarding the above issues when leaving the counter will not
+              be solved.
+            </div>
+            <h2 className="text-center">3. DIAMOND VALUATION'S COMMITMENT</h2>
+            <div>
+              DIAMOND VALUATION is committed to properly implementing the content of the appraisal
+              Agreement with the Customer based on knowledge, technical capacity,
+              existing expertise to provide the best Inspection services for
+              Customers have full responsibility in accordance with regulations
+              current. DIAMOND VALUATION is committed to protecting all personal information
+              Causes and results of Customer's inspection, no information is provided
+              about the Expert letting any third party know without consent
+              in writing by the Customer, except as otherwise provided by law
+              the law.
+            </div>
+          </div>
         </Col>
         <Col md={4}>
-          <h2 className="" style={{ marginTop: "200px", marginLeft: "50px" }}>
+          <h2 className="" style={{ marginTop: "200px",marginBottom:"50px", textAlign: "center" }}>
             Clear Policy - Loyal Customers
           </h2>
           <Image
@@ -136,13 +136,16 @@ const ServicePolicy = () => {
             fluid
             style={{
               borderRadius: "4px",
-              marginTop: "10ppx",
+              marginTop: "10px",
               width: "50%",
-              marginLeft: "130px",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+               marginBottom:"300px"
             }}
           />
-          <h2 className="" style={{ marginTop: "100px", marginLeft: "50px" }}>
-            Diamond Valuation- Life Valuation
+          <h2 className="" style={{ marginTop: "100px",marginBottom:"50px", textAlign: "center" }}>
+            Diamond Valuation Life Valuation
           </h2>
           <Image
             src="/src/assets/assetsCustomer/diamond.png"
@@ -151,10 +154,29 @@ const ServicePolicy = () => {
               borderRadius: "4px",
               marginTop: "10px",
               width: "50%",
-              marginLeft: "130px",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginBottom:"300px"
+            }}
+          />
+          <h2 className="" style={{ marginTop: "100px",marginBottom:"50px", textAlign: "center" }}>
+          Quality Comes with Prestige
+          </h2>
+          <Image
+            src="/src/assets/assetsCustomer/quantity.png"
+            fluid
+            style={{
+              borderRadius: "4px",
+              marginTop: "10px",
+              width: "50%",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
             }}
           />
         </Col>
+        
       </Row>
     </div>
   );

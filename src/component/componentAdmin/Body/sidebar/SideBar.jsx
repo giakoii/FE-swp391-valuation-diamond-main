b7e7d9a3fdcdc5d33d6ff50f68e2b010a2f62859
AnhMinh <minhnase173546@fpt.edu.vn>
2024-07-03 01:34:42 +0700

    de lai ban dau

--- src/component/componentAdmin/Body/sidebar/SideBar.jsx
+++ src/component/componentAdmin/Body/sidebar/SideBar.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
-import { FaCaretDown, FaCaretUp } from 'react-icons/fa';
-import { Badge } from 'react-bootstrap';
-import housedoor from 'assets/assetsAdmin/housedoor.svg';
-import people from 'assets/assetsAdmin/people.svg';
-import calendar from 'assets/assetsAdmin/calender.svg';
-import board from 'assets/assetsAdmin/board.svg';
-import '../../../componentAdmin/Body/sidebar/SideBarAdmin.css';
+  import React, { useEffect, useState } from 'react';
+  import Nav from 'react-bootstrap/Nav';
+  import Navbar from 'react-bootstrap/Navbar';
+  import { NavLink } from 'react-router-dom';
+  import { FaCaretDown, FaCaretUp } from 'react-icons/fa';
+  import { Badge } from 'react-bootstrap';
+  import '../../../componentAdmin/Body/sidebar/SideBarAdmin.css';
 
-const AdminSideBar = () => {
-  const [manageAccountOpen, setManageAccountOpen] = useState(false);
-  const [manageScheduleCount, setManageScheduleCount] = useState(0);
+  const AdminSideBar = () => {
+    const [manageAccountOpen, setManageAccountOpen] = useState(false);
+    const [manageScheduleCount, setManageScheduleCount] = useState(0);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+    useEffect(() => {
+      fetchScheduleCount(); // Fetch schedule count on component mount
+    }, []);
+
     const fetchScheduleCount = async () => {
       try {
         const response = await fetch('https://valuation.techtheworld.id.vn/order_detail_request/countOrderDetailWithEvaluationStaffIdIsNull');
@@ -22,71 +22,71 @@ const AdminSideBar = () => {
           throw new Error('Failed to fetch schedule count');
         }
         const data = await response.json();
-        setManageScheduleCount(data.count);
+        setManageScheduleCount(data.count); 
+        console.log(data.count);
       } catch (error) {
         console.error('Error fetching schedule count:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    fetchScheduleCount();
-  }, []);
+    const toggleManageAccount = () => {
+      setManageAccountOpen(!manageAccountOpen);
+    };
 
-  const toggleManageAccount = () => {
-    setManageAccountOpen(!manageAccountOpen);
+    return (
+      <Navbar className="flex-column w-100" style={{ backgroundColor: '#2a3447', height: '100vh' }}>
+        <Nav className="flex-column w-100 p-2 menu fw-bold">
+          <NavLink className='nav-link admin mt-2 mx-2 py-2' to="/admin/dashboard">
+            <img
+              src='assets/assetsAdmin/housedoor.svg'
+              width='30'
+              height='30'
+              className='mx-3'
+              alt='housedoor'
+            />
+            HomePage
+          </NavLink>
+          <div className='nav-link admin mx-2 my-3' onClick={toggleManageAccount} style={{ cursor: 'pointer' }}>
+            <img
+              src='assets/assetsAdmin/people.svg'
+              width='30'
+              height='30'
+              className='mx-3'
+              alt='people'
+            />
+            Manage Account {manageAccountOpen ? <FaCaretUp /> : <FaCaretDown />}
+          </div>
+          {manageAccountOpen && (
+            <div className='pl-3 mx-4'>
+              <NavLink className='nav-link admin mx-4 p-1 px-5' to="/admin/managestaff">Manage Staff</NavLink>
+              <NavLink className='nav-link admin mx-4 p-1 px-5' to="/admin/managecustomer">Manage Customer</NavLink>
+            </div>
+          )}
+          <NavLink className='nav-link admin mx-2 my-3' to="/admin/manageschedule">
+            <img
+              src='assets/assetsAdmin/calender.svg'
+              width='30'
+              height='30'
+              className='mx-3'
+              alt='calender'
+            />
+            Manage Schedule  <Badge bg="warning" className='mx-5'>{manageScheduleCount}</Badge>
+          </NavLink>
+          <NavLink className='nav-link admin mx-2' to="/admin/manageservice">
+            <img
+              src='assets/assetsAdmin/board.svg'
+              width='30'
+              height='30'
+              className='mx-3'
+              alt='board'
+            />
+            Manage Service
+          </NavLink>
+        </Nav>
+      </Navbar>
+    );
   };
 
-  return (
-    <Navbar className="flex-column w-100" style={{ backgroundColor: '#2a3447', height: '100vh' }}>
-      <Nav className="flex-column w-100 p-2 menu fw-bold">
-        <NavLink className='nav-link admin mt-2 mx-2 py-2' to="/admin/dashboard">
-          <img
-            src={housedoor}
-            width='30'
-            height='30'
-            className='mx-3'
-            alt='housedoor'
-          />
-          HomePage
-        </NavLink>
-        <div className='nav-link admin mx-2 my-3' onClick={toggleManageAccount} style={{ cursor: 'pointer' }}>
-          <img
-            src={people}
-            width='30'
-            height='30'
-            className='mx-3'
-            alt='people'
-          />
-          Manage Account {manageAccountOpen ? <FaCaretUp /> : <FaCaretDown />}
-        </div>
-        {manageAccountOpen && (
-          <div className='pl-3 mx-4'>
-            <NavLink className='nav-link admin mx-4 p-1 px-5' to="/admin/managestaff">Manage Staff</NavLink>
-            <NavLink className='nav-link admin mx-4 p-1 px-5' to="/admin/managecustomer">Manage Customer</NavLink>
-          </div>
-        )}
-        <NavLink className='nav-link admin mx-2 my-3' to="/admin/manageschedule">
-          <img
-            src={calendar}
-            width='30'
-            height='30'
-            className='mx-3'
-            alt='calender'
-          />
-          Manage Schedule <Badge bg="warning" className='mx-5'>{manageScheduleCount}</Badge>
-        </NavLink>
-        <NavLink className='nav-link admin mx-2' to="/admin/manageservice">
-          <img
-            src={board}
-            width='30'
-            height='30'
-            className='mx-3'
-            alt='board'
-          />
-          Manage Service
-        </NavLink>
-      </Nav>
-    </Navbar>
-  );
-};
-
-export default AdminSideBar;
+  export default AdminSideBar;

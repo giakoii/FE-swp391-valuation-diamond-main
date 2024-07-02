1a3d06ef564a2aebfcaf33d1c20358c4b4c0e940
khanhtran <khanhtpse173570>
2024-07-03 01:35:05 +0700

    update Check Diamond

--- src/page/pageCustomer/CheckDiamond/DetailDiamondCheck.jsx
+++ src/page/pageCustomer/CheckDiamond/DetailDiamondCheck.jsx
@@ -16,7 +16,7 @@ const DetailDiamondCheck = () => {
     const fetchDiamondDetails = async () => {
       try {
         const response = await fetch(
-          `https://valuation.techtheworld.id.vn/getDiamondAssessments/${assess_id}`
+          `https://valuation.techtheworld.id.vn/diamond_assessment/getDiamondAssessmentById/${assess_id}`
         );
         if (!response.ok) {
           throw new Error("Diamond not found");

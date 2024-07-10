a600c5e3ef50231f845a041428818a095ddb448d
khanhtran <khanhtpse173570>
2024-07-10 08:16:23 +0700

    update something

--- src/page/pageStaff/ViewReciptListApplication/ViewReciptList.jsx
+++ src/page/pageStaff/ViewReciptListApplication/ViewReciptList.jsx
@@ -150,6 +150,7 @@ export const ViewReciptList = () => {
               onClick={handleSearch}>
                 Search
               </Button>
+              
             </Col>
           </Row>
         </Form>

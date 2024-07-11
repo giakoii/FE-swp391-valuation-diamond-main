e414aefce631c10488d16a2c01d1aadb5fac14b4
khanhtran <khanhtpse173570@fpt.edu.vn>
2024-07-11 23:53:31 +0700

    Update Status View All Order

--- src/component/Status.jsx
+++ src/component/Status.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
-
-export const Status = ({ status }) => {
-    
-    const styleStatus = {
-        color: (status === 'Requesting') ? '#7C9099' :
-               (status === 'Accepted' || status === 'Finished') ? 'green' :
-               (status === 'Canceled' || status === 'DISABLE') ? 'red' :
-               (status === 'Assigned') ? '#00BCD4' :
-               (status === 'Sealed') ? '#9900EF' :
-               (status === 'Completed') ? 'orange' :
-               (status === 'In-Progress') ? '#2196F3' :
-               (status === 'ENABLE') ? 'green' : 'black',
-        border: 'solid',
-        borderRadius: '15px',
-        padding: '2px 8px'
-    };
-    return <span style={styleStatus}>{status}</span>;
-};
-
+import React from "react";
+ 
+export const Status = ({ status }) => { 
+  const styleStatus = { 
+    color: 
+      status === "Requesting"
+        ? "#7C9099"
+        : status === "Accepted" || status === "Finished"
+        ? "green"
+        : status === "Canceled" || status === "DISABLE"
+        ? "red"
+        : status === "Assigned"
+        ? "#00BCD4"
+        : status === "Sealed"
+        ? "#9900EF"
+        : status === "Completed"
+        ? "orange"
+        : status === "In-Progress"
+        ? "#2196F3"
+        : status === "ENABLE"
+        ? "green"
+        : "black",
+    border: "solid",
+    borderRadius: "15px",
+    padding: "2px 8px",
+  };
+  return <span style={styleStatus}>{status}</span>;
+}; 
